44bd13ba8ebf53761f33f45f926484b21dcce7e7
LucaDuyDang <danghoangduy120300@gmail.com>
2026-04-01 12:51:45 +0700

    clear file don't use, extra theme file for 2 ver

---
+++ wp-content/themes/tlab-v1/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>TLAB Shoe Spa</title>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+
+    <header class="header">
+        <div class="container">
+            <h2>TLAB</h2>
+        </div>
+    </header>
