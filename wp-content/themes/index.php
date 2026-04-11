59f923fd318550d04b6faf82692985d8f0eb519e
LucaDuyDang <danghoangduy120300@gmail.com>
2026-04-12 00:35:35 +0700

    fix test folder

--- wp-content/themes/index.php
+++ wp-content/themes/index.php
@@ -1,2 +1,4 @@
 <?php
 // Silence is golden.
+// This file is used to prevent directory listing on some servers when the directory is accessed directly.
+// You can add any custom code here if needed, but it's generally recommended to keep it empty
